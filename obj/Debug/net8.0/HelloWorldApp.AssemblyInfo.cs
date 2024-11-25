d051a1bb3eb3d19eadbde3c73de050b663fc3d19
unknown <sinambelaian@gmail.com>
2024-11-25 17:56:33 +0000

    Add UI

--- obj/Debug/net8.0/HelloWorldApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorldApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f365b7645136777048d191378ae108dd2de55a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a44e520033a06349089cfe33703ccccaadec7ec9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
