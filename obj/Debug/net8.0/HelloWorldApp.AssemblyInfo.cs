5ca3371b12c42a37806722b009fab91fd335ce16
unknown <sinambelaian@gmail.com>
2024-11-25 18:57:17 +0000

    Test

--- obj/Debug/net8.0/HelloWorldApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorldApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffe28c286d1bf60b6466759b6881ae010ca1b19d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08d99faeff93a89d1209dea9e8f15d1e46a9aa5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
